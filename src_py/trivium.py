e0b89922089888a5aa4817e75374f71cfa53a2bf
NanduBC <b.c.nandu17@gmail.com>
2020-07-17 20:08:54 +0530

    Implement Trivium as per design

---
+++ src_py/trivium.py
@@ -0,0 +1,38 @@
+import time
+
+reg_A = [0] * 93
+reg_B = [0] * 84
+reg_C = [0] * 111
+
+reg_C[108] = reg_C[109] = reg_C[110] = 1
+
+key = '00101110110000000110010101111101000011011100110100101111011001010101110010011001'
+init_vector = '01001111010101010111110111010110101011110010110100001100010000010111101100100000'
+
+# Initialising key and init vector
+reg_A[:80] = list(map(int, list(key)))
+reg_B[:80] = list(map(int, list(init_vector)))
+
+start_time = time.time_ns()
+# Implementation as per Trivium source code
+for i in range(1, 100001):
+    t1 = reg_A[65] ^ reg_A[92]
+    t2 = reg_B[68] ^ reg_B[83]
+    t3 = reg_C[65] ^ reg_C[110]
+
+    T1 = t1 ^ (reg_A[90] & reg_A[91]) ^ reg_B[76]
+    T2 = t2 ^ (reg_B[81] & reg_B[82]) ^ reg_C[87]
+    T3 = t3 ^ (reg_C[109] & reg_C[110]) ^ reg_A[68]
+    for x in range(92, 0, -1):
+        reg_A[x] = reg_A[x-1]
+    reg_A[0] = T1
+    for x in range(83, 0, -1):
+        reg_B[x] = reg_B[x-1]
+    reg_B[0] = T2
+    for x in range(110, 0, -1):
+        reg_C[x] = reg_C[x-1]
+    reg_C[0] = T3
+    if i in [32, 144, 1152, 100000]:
+        time_taken = (time.time_ns() - start_time)/10**9
+        print('Time taken {} for iteration {}'.format(
+            time_taken, i))
