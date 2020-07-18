e70f7a144ca464e4b594dde78c874189d0abbc51
NanduBC <b.c.nandu17@gmail.com>
2020-07-18 11:08:13 +0530

    Add apply_CA_to_blocks

--- src_py/pentavium.py
+++ src_py/pentavium.py
@@ -21,15 +21,16 @@ def five_neighborhood_cellular_automata(
 
     Parameters:
     ----------
-    s_minus_two:
-    s_minus_one:
-    s:
-    s_plus_one:
-    s_plus_two:
+    s_minus_two: State of left-left cell at i th clock cycle
+    s_minus_one: State of left cell at i th clock cycle
+    s: State of the cell at i th clock cycle
+    s_plus_one: State of right cell at i th clock cycle
+    s_plus_two: State of right-right cell at i th clock cycle
+    rule_index: Rule mapping
 
     Returns:
     -------
-    State of s in the next clock cycle
+    State of s in the i+1 th clock cycle
     '''
     if rule_index == 1:
         return s_minus_two ^ s_minus_one ^ s ^ s_plus_one ^ s_plus_two
@@ -48,8 +49,24 @@ def apply_CA_to_blocks(reg_X):
     Parameters:
     ----------
     reg_X: reg_A, reg_B, reg_C
+
+    Notes:
+    -----
+    Since CA is null boundary first, second, second last and
+    last elements might have some neighbors as 0
     '''
-    pass
+    for i in range(len(reg_X)):
+        rule_index = ca_rule_order[i%8]
+        if i == 0:
+            reg_X[i] == five_neighborhood_cellular_automata(0, 0, reg_X[i], reg_X[i+1], reg_X[i+2], rule_index)
+        elif i == 1:
+            reg_X[i] == five_neighborhood_cellular_automata(0, reg_X[i-1], reg_X[i], reg_X[i+1], reg_X[i+2], rule_index)
+        elif i == (len(reg_X) - 2):
+            reg_X[i] == five_neighborhood_cellular_automata(reg_X[i-2], reg_X[i-1], reg_X[i], reg_X[i+1], 0, rule_index)
+        elif i == (len(reg_X) - 1):
+            reg_X[i] == five_neighborhood_cellular_automata(reg_X[i-2], reg_X[i-1], reg_X[i], 0, 0, rule_index)
+        else:
+            reg_X[i] == five_neighborhood_cellular_automata(reg_X[i-2], reg_X[i-1], reg_X[i], reg_X[i+1], reg_X[i+2], rule_index)
 
 
 reg_A = [0] * 93
@@ -77,14 +94,17 @@ for i in range(1, 100001):
     T3 = t3 ^ (reg_C[109] & reg_C[110]) ^ reg_A[68]
 
     reg_A.pop()
+    apply_CA_to_blocks(reg_A)
     reg_A.insert(0, T1)
     reg_B.pop()
+    apply_CA_to_blocks(reg_B)
     reg_B.insert(0, T2)
     reg_C.pop()
+    apply_CA_to_blocks(reg_C)
     reg_C.index(0, T3)
 
     output_bit = t1 ^ t2 ^ t3
     if i in [32, 144, 1152, 100000]:
         time_taken = (time.time_ns() - start_time)/10**9
-        print('Time taken {} for iteration {}'.format(
+        print('Time taken {} for clock cycle {}'.format(
             time_taken, i))
