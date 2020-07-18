96640433c6c9832ccb35839abeaeec733a6a5523
NanduBC <b.c.nandu17@gmail.com>
2020-07-18 16:39:26 +0530

    Fix bugs and incorporate code review changes in pentavium

--- src_py/pentavium.py
+++ src_py/pentavium.py
@@ -1,5 +1,5 @@
 '''
-This is a Python implementation of Pentavium
+This is a naive Python implementation of Pentavium
 '''
 import time
 
@@ -10,6 +10,7 @@ print('PENTAVIUM')
 # 3 --> 1452976485
 # 4 --> 1520018790
 ca_rule_order = [3, 1, 2, 4, 1, 3, 4, 2]
+treg_X = [0] * 110
 
 
 def five_neighborhood_cellular_automata(
@@ -18,7 +19,7 @@ def five_neighborhood_cellular_automata(
         s,
         s_plus_one,
         s_plus_two,
-        rule_index):
+        rule_mappings):
     '''
     Find the next state of cell s based on the values of s_minus_two,
     s_minus_one, s, s_plus_one and s_plus_two and the rule.
@@ -30,25 +31,27 @@ def five_neighborhood_cellular_automata(
     s: State of the cell at i th clock cycle
     s_plus_one: State of right cell at i th clock cycle
     s_plus_two: State of right-right cell at i th clock cycle
-    rule_index: Rule mapping
+    rule_mappings: Shorthand for rule numbers that would be used to
+        determine the rule to be used at each cell
 
     Returns:
     -------
     State of s in the i+1 th clock cycle
     '''
-    if rule_index == 1:
+    if rule_mappings == 1:
         return s_minus_two ^ s_minus_one ^ s ^ s_plus_one ^ s_plus_two
-    elif rule_index == 2:
-        return s_minus_two ^ s_minus_two ^ s_plus_one ^ s_plus_two
-    elif rule_index == 3:
+    elif rule_mappings == 2:
+        return s_minus_two ^ s_minus_one ^ s_plus_one ^ s_plus_two
+    elif rule_mappings == 3:
         return s_plus_one & (1 ^ s_plus_two ^ s_minus_two ^ s_minus_one) | (1 ^ s_plus_one) & (1 ^ s_plus_two ^ s_minus_two ^ s)
-    elif rule_index == 4:
+    elif rule_mappings == 4:
         return (1 ^ s_minus_one) & (s_minus_two ^ s_plus_one ^ s_plus_two) | s_minus_one & (1 ^ s_minus_two ^ s ^ s_plus_two)
 
 
 def apply_CA_to_blocks(reg_X):
     '''
     Apply approapriate CA rule to each cell of the block registers
+    except the last register
 
     Parameters:
     ----------
@@ -59,18 +62,17 @@ def apply_CA_to_blocks(reg_X):
     Since CA is null boundary first, second, second last and
     last elements might have some neighbors as 0
     '''
-    for i in range(len(reg_X)):
-        rule_index = ca_rule_order[i%8]
+    for i in range(len(reg_X)-1):
+        rule_mappings = ca_rule_order[i % 8]
         if i == 0:
-            reg_X[i] == five_neighborhood_cellular_automata(0, 0, reg_X[i], reg_X[i+1], reg_X[i+2], rule_index)
+            treg_X[i] = five_neighborhood_cellular_automata(0, 0, reg_X[i], reg_X[i+1], reg_X[i+2], rule_mappings)
         elif i == 1:
-            reg_X[i] == five_neighborhood_cellular_automata(0, reg_X[i-1], reg_X[i], reg_X[i+1], reg_X[i+2], rule_index)
+            treg_X[i] = five_neighborhood_cellular_automata(0, reg_X[i-1], reg_X[i], reg_X[i+1], reg_X[i+2], rule_mappings)
         elif i == (len(reg_X) - 2):
-            reg_X[i] == five_neighborhood_cellular_automata(reg_X[i-2], reg_X[i-1], reg_X[i], reg_X[i+1], 0, rule_index)
-        elif i == (len(reg_X) - 1):
-            reg_X[i] == five_neighborhood_cellular_automata(reg_X[i-2], reg_X[i-1], reg_X[i], 0, 0, rule_index)
+            treg_X[i] = five_neighborhood_cellular_automata(reg_X[i-2], reg_X[i-1], reg_X[i], reg_X[i+1], 0, rule_mappings)
         else:
-            reg_X[i] == five_neighborhood_cellular_automata(reg_X[i-2], reg_X[i-1], reg_X[i], reg_X[i+1], reg_X[i+2], rule_index)
+            treg_X[i] = five_neighborhood_cellular_automata(reg_X[i-2], reg_X[i-1], reg_X[i], reg_X[i+1], reg_X[i+2], rule_mappings)
+    return treg_X[:len(reg_X)-1]
 
 
 reg_A = [0] * 93
@@ -93,21 +95,19 @@ for i in range(1, 100001):
     t2 = reg_B[68] ^ reg_B[83]
     t3 = reg_C[65] ^ reg_C[110]
 
-    T1 = t1 ^ (reg_A[90] & reg_A[91]) ^ reg_B[76]
-    T2 = t2 ^ (reg_B[81] & reg_B[82]) ^ reg_C[87]
-    T3 = t3 ^ (reg_C[109] & reg_C[110]) ^ reg_A[68]
-
-    reg_A.pop()
-    apply_CA_to_blocks(reg_A)
-    reg_A.insert(0, T1)
-    reg_B.pop()
-    apply_CA_to_blocks(reg_B)
-    reg_B.insert(0, T2)
-    reg_C.pop()
-    apply_CA_to_blocks(reg_C)
-    reg_C.insert(0, T3)
-
     output_bit = t1 ^ t2 ^ t3
+
+    t1 = t1 ^ (reg_A[90] & reg_A[91]) ^ reg_B[76]
+    t2 = t2 ^ (reg_B[81] & reg_B[82]) ^ reg_C[87]
+    t3 = t3 ^ (reg_C[109] & reg_C[110]) ^ reg_A[68]
+
+    reg_A[1:] = apply_CA_to_blocks(reg_A)
+    reg_A[0] = t3
+    reg_B[1:] = apply_CA_to_blocks(reg_B)
+    reg_B[0] = t1
+    reg_C[1:] = apply_CA_to_blocks(reg_C)
+    reg_C[0] = t2
+
     if i in [32, 144, 1152, 100000]:
         time_taken = (time.time_ns() - start_time)/10**9
         print('Time taken {} for clock cycle {}'.format(
