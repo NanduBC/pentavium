92de4a6eec4a8d57564795b0edff1db66d186505
NanduBC <b.c.nandu17@gmail.com>
2020-08-07 20:07:39 +0530

    Make time comparison max iteration configurable

--- time_comparison.c
+++ time_comparison.c
@@ -1,11 +1,13 @@
+#include <stdlib.h>
 #include "src_c/trivium_naive.c"
 #include "src_c/cavium_naive.c"
 #include "src_c/pentavium_naive.c"
 
-int main(){
+int main(int argc, char* argv[]){
 	int key[80] = {0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1};
 	int iv[80] = {0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0};
-	static int keystream[1000];
+	int max_iterations = atoi(argv[1]);
+	int keystream[max_iterations];
 	printf("TRIVIUM C IMPLEMENTATION\n");
 	trivium_keystream_generation(keystream, key, iv, sizeof(keystream)/sizeof(int));
 	printf("Keystream generated\n");
--- time_comparison_script.py
+++ time_comparison_script.py
@@ -1,8 +1,10 @@
+import sys
+
 import src_py.pentavium as pentavium
 import src_py.cavium as cavium
 import src_py.trivium as trivium
 
-MAX_ITERATIONS = 1000
+MAX_ITERATIONS = int(sys.argv[1])
 LOG_ITERATIONS_LIST = [32, 144, 1152, MAX_ITERATIONS]
 
 trivium_keystream = trivium.run_trivium(MAX_ITERATIONS, LOG_ITERATIONS_LIST)[:100]
